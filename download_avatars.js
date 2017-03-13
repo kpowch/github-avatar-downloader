a37ebc235017fe6edc0404f28c84c67838298b70
Ksenia Owchar <ksenia.owchar@gmail.com>
2017-03-13 23:13:24 +0000

    defined constants and verified that the requestURL is returning a JSON contributor list

--- download_avatars.js
+++ download_avatars.js
@@ -1,14 +1,16 @@
 // require the request module
 var request = require('request');
 
-// var endpoint = 'https://api.github.com/repos/jquery/jquery/contributors';
-
 console.log('Welcome to the GitHub Avatar Downloader!!')
+
+var GITHUB_USER = "kpowch";
+var GITHUB_TOKEN = "b23281cab5c0a4a15f1cd9c82ef317201c2a9359";
 
 // use request library to programmatically fetch the list of contributors
 // via HTTPS for given repo.
 function getRepoContributors(repoOwner, repoName, cb) {
-
+  var requestURL = 'https://'+ GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
+  console.log(requestURL);
 }
 
 getRepoContributors("jquery", "jquery", function(err, result) {
