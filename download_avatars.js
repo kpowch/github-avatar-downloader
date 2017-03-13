6b47b1763087a7fc224eaca65c63b4331ec8c446
Ksenia Owchar <ksenia.owchar@gmail.com>
2017-03-13 21:57:15 +0000

    added empty function that will eventaully fetch list of contributors via https

--- download_avatars.js
+++ download_avatars.js
@@ -1,4 +1,17 @@
 // require the request module
 var request = require('request');
 
+// var endpoint = 'https://api.github.com/repos/jquery/jquery/contributors';
+
 console.log('Welcome to the GitHub Avatar Downloader!!')
+
+// use request library to programmatically fetch the list of contributors
+// via HTTPS for given repo.
+function getRepoContributors(repoOwner, repoName, cb) {
+
+}
+
+getRepoContributors("jquery", "jquery", function(err, result) {
+  console.log("Errors:", err);
+  console.log("Result:", result);
+});
