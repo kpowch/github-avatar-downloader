9d180b02f0997f13b661f902ded09c215b04df15
Ksenia Owchar <ksenia.owchar@gmail.com>
2017-03-13 23:50:20 +0000

    successfully made HTTP request using request function to print JSON object of contibutorsgit status

--- download_avatars.js
+++ download_avatars.js
@@ -1,16 +1,33 @@
 // require the request module
 var request = require('request');
 
-console.log('Welcome to the GitHub Avatar Downloader!!')
+console.log('Welcome to the GitHub Avatar Downloader!!');
 
 var GITHUB_USER = "kpowch";
 var GITHUB_TOKEN = "b23281cab5c0a4a15f1cd9c82ef317201c2a9359";
 
+
 // use request library to programmatically fetch the list of contributors
 // via HTTPS for given repo.
 function getRepoContributors(repoOwner, repoName, cb) {
-  var requestURL = 'https://'+ GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
+  var requestURL = 'https://' + GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
   console.log(requestURL);
+
+  // https://github.com/request/request#custom-http-headers
+  var options = {
+    url: requestURL,
+    headers: {
+      'User-Agent': 'GitHub Avatar Downloader - Student Project'
+    }
+  };
+
+  request(options, function (err, response, body) {
+    if (err) {
+      throw err;
+    }
+    console.log('statusCode:', response && response.statusCode);
+    console.log(JSON.parse(body));
+  });
 }
 
 getRepoContributors("jquery", "jquery", function(err, result) {
