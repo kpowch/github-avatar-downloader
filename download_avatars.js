38e42fe629b5fe2469057e25694082436369fcc6
Ksenia Owchar <ksenia.owchar@gmail.com>
2017-03-14 05:32:13 +0000

    Used environment variables for github user and token

--- download_avatars.js
+++ download_avatars.js
@@ -1,20 +1,17 @@
 var request = require('request');
 var fs = require('fs');
+require('dotenv').config();
 
 var repoOwner = process.argv[2];
 var repoName = process.argv[3];
 
 console.log('Welcome to the GitHub Avatar Downloader!!');
 
-var GITHUB_USER = "kpowch";
-var GITHUB_TOKEN = "b23281cab5c0a4a15f1cd9c82ef317201c2a9359";
-
-
 // Makes a request for JSON, getting back an array of contributors via HTTPS for given repo.
 // Passes data to cb, an anonymous callback function that it's given.
 function getRepoContributors(repoOwner, repoName, cb) {
-  var requestURL = 'https://' + GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
-  // console.log(requestURL);
+  // builds request from the environment variables in the .env file
+  var requestURL = 'https://' + process.env.GITHUB_USER + ':' + process.env.GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
 
   // refer to good resource: https://github.com/request/request#custom-http-headers
   var options = {
