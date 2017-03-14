17f8216f7ae08b069ec8fe5672a3d6af3ee89284
Ksenia Owchar <ksenia.owchar@gmail.com>
2017-03-14 00:47:00 +0000

    created downloadImagebyURL function that can now download an image into a folder

--- download_avatars.js
+++ download_avatars.js
@@ -1,5 +1,6 @@
-// require the request module
+// require 'request' and the Node 'fs' (filesystem) modules
 var request = require('request');
+var fs = require('fs');
 
 console.log('Welcome to the GitHub Avatar Downloader!!');
 
@@ -11,9 +12,9 @@ var GITHUB_TOKEN = "b23281cab5c0a4a15f1cd9c82ef317201c2a9359";
 // via HTTPS for given repo.
 function getRepoContributors(repoOwner, repoName, cb) {
   var requestURL = 'https://' + GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
-  console.log(requestURL);
+  // console.log(requestURL);
 
-  // https://github.com/request/request#custom-http-headers
+  // refer to good resource: https://github.com/request/request#custom-http-headers
   var options = {
     url: requestURL,
     headers: {
@@ -29,18 +30,40 @@ function getRepoContributors(repoOwner, repoName, cb) {
 
     var contributorObj = JSON.parse(body);
 
-    cb(contributorObj);
+    cb(err, contributorObj);
   });
 }
 
-getRepoContributors("jquery", "jquery", function(object) {
-  for (prop in object) {
-    console.log(object[prop].avatar_url);
+getRepoContributors("jquery", "jquery", function(err, result) {
+  console.log('Errors:', err);
+  var arrayOfURLS = [];
+  for (prop in result) {
+    //console.log(result[prop].avatar_url);
+    arrayOfURLS.push(result[prop].avatar_url);
   }
+  // console.log(arrayOfURLS);
+  return arrayOfURLS;
 });
 
+function downloadImageByURL(url, filePath) {
+  request.get(url)
+       .on('error', function (err) {
+         throw err;
+       })
+       .on('data', function (data) {
+         //console.log('Getting data...');
+       })
+       .on('end', function () {
+         console.log('Downloading image...');
+       })
+       .on('response', function (response) {
+         // console.log('Response Status Code:', response.statusCode);
+         // console.log('Content type:', response.headers['content-type']);
+       })
+       .pipe(fs.createWriteStream(filePath))
+       .on('finish', function() {
+         console.log('Download complete!');
+       });
+}
 
-// getRepoContributors("jquery", "jquery", function(err, result) {
-//   console.log("Errors:", err);
-//   console.log("Result:", result);
-// });
+downloadImageByURL("https://avatars2.githubusercontent.com/u/2741?v=3&s=466", "avatars/kvirani.jpg")
